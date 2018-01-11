159465be6afd07b3b5e19194b96c0628f783dda4
Uğur Öztürk <mail@uozturk.com>
2018-01-11 23:37:22 +0300

    Default example schema changed

--- example.py
+++ example.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import sys
 import time
 
-sys.path.append(os.path.join(sys.path[0], 'src'))
-
-from check_status import check_status
-from feed_scanner import feed_scanner
-from follow_protocol import follow_protocol
-from instabot import InstaBot
-from unfollow_protocol import unfollow_protocol
+from src import InstaBot
+from src.check_status import check_status
+from src.feed_scanner import feed_scanner
+from src.follow_protocol import follow_protocol
+from src.unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
     login="username",
